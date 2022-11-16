6635b7c832d04face666b0c00872de5e8dc403c0
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-11-16 13:09:08 +0800

    sadf
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- pkg/pluginManager/controllerTools.go
+++ pkg/pluginManager/controllerTools.go
@@ -124,40 +124,46 @@ func (s *pluginControllerReconciler) UpdateStatus(logger *zap.Logger, ctx contex
 	case nowTime.After(latestRecord.StartTimeStamp.Time) && nowTime.Before(latestRecord.DeadLineTimeStamp.Time):
 		nextInterval := time.Duration(types.ControllerConfig.Configmap.TaskPollIntervalInSecond) * time.Second
 
-		if finished, existed := s.taskRoundData.CheckTask(taskName); !existed {
-			// ths task just begin, or the conroller pod just up ,record it
+		if _, existed := s.taskRoundData.CheckTask(taskName); !existed {
+			// this round just begin, or the controller pod just up ,record it
 			s.taskRoundData.SetTask(taskName, false)
+
 			// trigger after interval
 			result = &reconcile.Result{
 				RequeueAfter: nextInterval,
 			}
 		} else {
-			if finished {
-				// do nothing
-			} else {
-				// try do pull
+			// try do pull
+			if latestRecord.Status == crd.StatusHistoryRecordStatusOngoing {
 				logger.Debug("try to poll the status of task " + taskName)
+				if roundDone, e := s.UpdateRoundFinalStatus(logger, ctx, newStatus, false); e != nil {
+					return nil, nil, e
+				} else {
+					if roundDone {
+						logger.Sugar().Infof("round %v get reports from all agents ", roundNumber)
+						// TODO: add to workqueue to collect all report of last round, for node latestRecord.FailedAgentNodeList and latestRecord.SucceedAgentNodeList
 
-				if latestRecord.Status == crd.StatusHistoryRecordStatusOngoing {
-					if roundDone, e := s.UpdateRoundFinalStatus(logger, ctx, newStatus, false); e != nil {
-						return nil, nil, e
+						// trigger when deadline
+						result = &reconcile.Result{
+							RequeueAfter: latestRecord.DeadLineTimeStamp.Time.Sub(time.Now()),
+						}
 					} else {
-						if roundDone {
-							logger.Sugar().Infof("round %v get reports from all agents ", roundNumber)
-							// mark round finish, will not check round status anymore
-							s.taskRoundData.SetTask(taskName, true)
-
-							// TODO: add to workqueue to collect all report of last round, for node latestRecord.FailedAgentNodeList and latestRecord.SucceedAgentNodeList
-
+						// trigger after interval
+						result = &reconcile.Result{
+							RequeueAfter: nextInterval,
 						}
 					}
 				}
-
+			} else {
+				logger.Debug("ignore poll the status of task " + taskName)
+				// mark round finish, will not check round status anymore
+				s.taskRoundData.SetTask(taskName, true)
 				// trigger when deadline
 				result = &reconcile.Result{
 					RequeueAfter: latestRecord.DeadLineTimeStamp.Time.Sub(time.Now()),
 				}
 			}
+
 		}
 
 	case nowTime.Before(latestRecord.StartTimeStamp.Time):
