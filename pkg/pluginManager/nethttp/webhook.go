4f54bcf62a5c813c836066c91c0e81de61cce28a
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-11-18 15:33:59 +0800

    sadf
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- pkg/pluginManager/nethttp/webhook.go
+++ pkg/pluginManager/nethttp/webhook.go
@@ -75,18 +75,80 @@ func (s *PluginNetHttp) WebhookValidateCreate(logger *zap.Logger, ctx context.Co
 		logger.Error(s)
 		return apierrors.NewBadRequest(s)
 	}
-	logger.Sugar().Infof("obj: %+v", r)
+	logger.Sugar().Debugf("Nethttp: %+v", r)
 
-	// TODO: validate request
-	if r.Spec.Request.QPS >= types.ControllerConfig.Configmap.NethttpDefaultRequestMaxQps {
-		s := fmt.Sprintf("nethttp %v requires qps %v bigger than maximum %v", r.Name, r.Spec.Request.QPS, types.ControllerConfig.Configmap.NethttpDefaultRequestMaxQps)
-		logger.Error(s)
-		return apierrors.NewBadRequest(s)
+	// validate Schedule
+	if true {
+		if err := tools.ValidataCrdSchedule(r.Spec.Schedule); err != nil {
+			s := fmt.Sprintf("nethttp %v : %v", r.Name, err)
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
+	}
+
+	// validate request
+	if true {
+		if r.Spec.Request.QPS >= types.ControllerConfig.Configmap.NethttpDefaultRequestMaxQps {
+			s := fmt.Sprintf("nethttp %v requires qps %v bigger than maximum %v", r.Name, r.Spec.Request.QPS, types.ControllerConfig.Configmap.NethttpDefaultRequestMaxQps)
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
+		if r.Spec.Request.PerRequestTimeoutInSecond > r.Spec.Request.DurationInSecond {
+			s := fmt.Sprintf("nethttp %v requires PerRequestTimeoutInSecond %vs smaller than DurationInSecond %vs ", r.Name, r.Spec.Request.PerRequestTimeoutInSecond, r.Spec.Request.DurationInSecond)
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
+		if r.Spec.Request.DurationInSecond > int(r.Spec.Schedule.TimeoutMinute*60) {
+			s := fmt.Sprintf("nethttp %v requires request.DurationInSecond %vs smaller than Schedule.TimeoutMinute %vm ", r.Name, r.Spec.Request.DurationInSecond, r.Spec.Schedule.TimeoutMinute)
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
+	}
+
+	// validate target
+	if true {
+		if r.Spec.Target.TargetAgent == nil && r.Spec.Target.TargetUrl == nil {
+			s := fmt.Sprintf("nethttp %v, no target specified in the spec", r.Name)
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
+
+		// validate target
+		if r.Spec.Target.TargetAgent.TestIPv4 != nil && *(r.Spec.Target.TargetAgent.TestIPv4) && !types.ControllerConfig.Configmap.EnableIPv4 {
+			s := fmt.Sprintf("nethttp %v TestIPv4, but spiderdoctor ipv4 feature is disabled", r.Name)
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
+		if r.Spec.Target.TargetAgent.TestIPv6 != nil && *(r.Spec.Target.TargetAgent.TestIPv6) && !types.ControllerConfig.Configmap.EnableIPv6 {
+			s := fmt.Sprintf("nethttp %v TestIPv6, but spiderdoctor ipv6 feature is disabled", r.Name)
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
+	}
+
+	// validate SuccessCondition
+	if true {
+		if r.Spec.SuccessCondition.SuccessRate == nil && r.Spec.SuccessCondition.MeanAccessDelayInMs == nil {
+			s := fmt.Sprintf("nethttp %v, no SuccessCondition specified in the spec", r.Name)
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
+		if r.Spec.SuccessCondition.SuccessRate != nil && (*(r.Spec.SuccessCondition.SuccessRate) > 1) {
+			s := fmt.Sprintf("nethttp %v, SuccessCondition.SuccessRate %v must not be bigger than 1", r.Name, *(r.Spec.SuccessCondition.SuccessRate))
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
+		if r.Spec.SuccessCondition.SuccessRate != nil && (*(r.Spec.SuccessCondition.SuccessRate) < 0) {
+			s := fmt.Sprintf("nethttp %v, SuccessCondition.SuccessRate %v must not be smaller than 0 ", r.Name, *(r.Spec.SuccessCondition.SuccessRate))
+			logger.Error(s)
+			return apierrors.NewBadRequest(s)
+		}
 	}
 
 	return nil
 }
 
+// this will not be called, it is not allowed to modify crd
 func (s *PluginNetHttp) WebhookValidateUpdate(logger *zap.Logger, ctx context.Context, oldObj, newObj runtime.Object) error {
 	r, ok := newObj.(*crd.Nethttp)
 	if !ok {
--- pkg/pluginManager/tools/tools.go
+++ pkg/pluginManager/tools/tools.go
@@ -1,10 +1,29 @@
 package tools
 
 import (
+	"fmt"
 	crd "github.com/spidernet-io/spiderdoctor/pkg/k8s/apis/spiderdoctor.spidernet.io/v1"
 )
 
 func ValidataCrdSchedule(plan *crd.SchedulePlan) error {
+
+	if plan == nil {
+		return fmt.Errorf("Schedule is empty ")
+	}
+
+	if plan.StartAfterMinute < 0 {
+		return fmt.Errorf("Schedule.StartAfterMinute %v must not be smaller than 0 ", plan.StartAfterMinute)
+	}
+
+	if plan.TimeoutMinute < 1 {
+		return fmt.Errorf("Schedule.TimeoutMinute %v must not be smaller than 1 ", plan.TimeoutMinute)
+	}
+	if plan.IntervalMinute < 1 {
+		return fmt.Errorf("Schedule.IntervalMinute %v must not be smaller than 1 ", plan.IntervalMinute)
+	}
+	if plan.TimeoutMinute > plan.IntervalMinute {
+		return fmt.Errorf("Schedule.TimeoutMinute %v must not be bigger than Schedule.IntervalMinute %v ", plan.TimeoutMinute, plan.IntervalMinute)
+	}
 
 	return nil
 }
