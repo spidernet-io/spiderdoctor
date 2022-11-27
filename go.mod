67bd455e66bb8e8f771db32f81faec76e31e1617
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-11-27 12:59:16 +0800

    dns load
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- go.mod
+++ go.mod
@@ -12,6 +12,8 @@ require (
 	github.com/go-swagger/go-swagger v0.30.3
 	github.com/google/gops v0.3.25
 	github.com/jessevdk/go-flags v1.5.0
+	github.com/miekg/dns v1.1.50
+	github.com/montanaflynn/stats v0.6.6
 	github.com/onsi/ginkgo/v2 v2.4.0
 	github.com/onsi/gomega v1.23.0
 	github.com/pkg/errors v0.9.1
@@ -27,6 +29,7 @@ require (
 	go.opentelemetry.io/otel/metric v0.33.0
 	go.opentelemetry.io/otel/sdk v1.11.1
 	go.opentelemetry.io/otel/sdk/metric v0.33.0
+	go.uber.org/ratelimit v0.2.0
 	go.uber.org/zap v1.23.0
 	golang.org/x/net v0.2.0
 	google.golang.org/grpc v1.50.1
@@ -46,6 +49,7 @@ require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.1.1 // indirect
 	github.com/Masterminds/sprig/v3 v3.2.2 // indirect
+	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
@@ -117,13 +121,13 @@ require (
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
-	golang.org/x/mod v0.6.0 // indirect
+	golang.org/x/mod v0.7.0 // indirect
 	golang.org/x/oauth2 v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/term v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	golang.org/x/time v0.2.0 // indirect
-	golang.org/x/tools v0.2.0 // indirect
+	golang.org/x/tools v0.3.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20221111202108-142d8a6fa32e // indirect
---
+++ pkg/loadRequest/dns_test.go
@@ -0,0 +1,136 @@
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+
+package loadRequest_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/miekg/dns"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/spidernet-io/spiderdoctor/pkg/loadRequest"
+	"github.com/spidernet-io/spiderdoctor/pkg/logger"
+)
+
+var _ = Describe("test dns ", Label("dns"), func() {
+
+	It("test udp ", func() {
+
+		dnsServer := "223.5.5.5:53"
+		req := &loadRequest.DnsRequestData{
+			Method:                loadRequest.RequestMethodUdp,
+			DnsType:               dns.TypeA,
+			TargetDomain:          "www.baidu.com",
+			DnsServerAddr:         &dnsServer,
+			PerRequestTimeoutInMs: 1000,
+			DurationInMs:          1000,
+			Qps:                   10,
+		}
+
+		log := logger.NewStdoutLogger("debug", "test")
+		result, e := loadRequest.DnsRequest(log, req)
+		Expect(e).NotTo(HaveOccurred(), "failed to execute , error=%v", e)
+		Expect(result.FailedCount).To(Equal(0))
+		Expect(len(result.ReplyCode)).To(Equal(1))
+		Expect(result.ReplyCode).Should(HaveKey(dns.RcodeToString[dns.RcodeSuccess]))
+
+		jsongByte, e := json.Marshal(result)
+		Expect(e).NotTo(HaveOccurred(), "failed to Marshal , error=%v", e)
+
+		var out bytes.Buffer
+		e = json.Indent(&out, jsongByte, "", "\t")
+		Expect(e).NotTo(HaveOccurred(), "failed to Indent , error=%v", e)
+		fmt.Printf("%s\n", out.String())
+
+	})
+
+	It("test tcp ", func() {
+
+		dnsServer := "223.5.5.5:53"
+		req := &loadRequest.DnsRequestData{
+			Method:                loadRequest.RequestMethodTcp,
+			DnsType:               dns.TypeA,
+			TargetDomain:          "www.baidu.com",
+			DnsServerAddr:         &dnsServer,
+			PerRequestTimeoutInMs: 1000,
+			DurationInMs:          1000,
+			Qps:                   10,
+		}
+
+		log := logger.NewStdoutLogger("debug", "test")
+		result, e := loadRequest.DnsRequest(log, req)
+		Expect(e).NotTo(HaveOccurred(), "failed to execute , error=%v", e)
+		Expect(result.FailedCount).To(Equal(0))
+		Expect(len(result.ReplyCode)).To(Equal(1))
+		Expect(result.ReplyCode).Should(HaveKey(dns.RcodeToString[dns.RcodeSuccess]))
+
+		jsongByte, e := json.Marshal(result)
+		Expect(e).NotTo(HaveOccurred(), "failed to Marshal , error=%v", e)
+
+		var out bytes.Buffer
+		e = json.Indent(&out, jsongByte, "", "\t")
+		Expect(e).NotTo(HaveOccurred(), "failed to Indent , error=%v", e)
+		fmt.Printf("%s\n", out.String())
+	})
+
+	It("test bad domain ", func() {
+
+		dnsServer := "223.5.5.5:53"
+		req := &loadRequest.DnsRequestData{
+			Method:                loadRequest.RequestMethodUdp,
+			DnsType:               dns.TypeA,
+			TargetDomain:          "www.no-existed.com",
+			DnsServerAddr:         &dnsServer,
+			PerRequestTimeoutInMs: 1000,
+			DurationInMs:          1000,
+			Qps:                   10,
+		}
+
+		log := logger.NewStdoutLogger("debug", "test")
+		result, e := loadRequest.DnsRequest(log, req)
+		Expect(e).NotTo(HaveOccurred(), "failed to execute , error=%v", e)
+		Expect(result.SucceedCount).To(Equal(0))
+		Expect(len(result.ReplyCode)).To(Equal(1))
+		Expect(result.ReplyCode).Should(HaveKey(dns.RcodeToString[dns.RcodeNameError]))
+
+		jsongByte, e := json.Marshal(result)
+		Expect(e).NotTo(HaveOccurred(), "failed to Marshal , error=%v", e)
+
+		var out bytes.Buffer
+		e = json.Indent(&out, jsongByte, "", "\t")
+		Expect(e).NotTo(HaveOccurred(), "failed to Indent , error=%v", e)
+		fmt.Printf("%s\n", out.String())
+
+	})
+
+	It("test aaaa ", Label("aaaa"), func() {
+		dnsServer := "223.5.5.5:53"
+		req := &loadRequest.DnsRequestData{
+			Method:                loadRequest.RequestMethodUdp,
+			DnsType:               dns.TypeAAAA,
+			TargetDomain:          "wikipedia.org",
+			DnsServerAddr:         &dnsServer,
+			PerRequestTimeoutInMs: 1000,
+			DurationInMs:          1000,
+			Qps:                   10,
+		}
+
+		log := logger.NewStdoutLogger("debug", "test")
+		result, e := loadRequest.DnsRequest(log, req)
+		Expect(e).NotTo(HaveOccurred(), "failed to execute , error=%v", e)
+		Expect(result.FailedCount).To(Equal(0))
+		Expect(len(result.ReplyCode)).To(Equal(1))
+		Expect(result.ReplyCode).Should(HaveKey(dns.RcodeToString[dns.RcodeSuccess]))
+
+		jsongByte, e := json.Marshal(result)
+		Expect(e).NotTo(HaveOccurred(), "failed to Marshal , error=%v", e)
+
+		var out bytes.Buffer
+		e = json.Indent(&out, jsongByte, "", "\t")
+		Expect(e).NotTo(HaveOccurred(), "failed to Indent , error=%v", e)
+		fmt.Printf("%s\n", out.String())
+
+	})
+})
